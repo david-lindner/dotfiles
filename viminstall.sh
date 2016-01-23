bc829d3d3dc811723afb33c7e9abcac8d1a03d8a
David Lindner <dev@davidlindner.me>
2016-01-23 21:05:25 +0100

    fixed supertab snipmate conflict, by switching to another snipmate repo

--- viminstall.sh
+++ viminstall.sh
@@ -11,8 +11,12 @@ git clone https://github.com/miyakogi/conoline.vim
 git clone https://github.com/kien/ctrlp.vim
 git clone https://github.com/scrooloose/nerdcommenter
 git clone https://github.com/scrooloose/nerdtree
-git clone https://github.com/msanders/snipmate.vim
 git clone https://github.com/ervandew/supertab
 git clone https://github.com/vim-airline/vim-airline
 git clone https://github.com/Townk/vim-autoclose
 git clone https://github.com/flazz/vim-colorschemes
+#snipmate
+git clone https://github.com/tomtom/tlib_vim.git
+git clone https://github.com/MarcWeber/vim-addon-mw-utils.git
+git clone https://github.com/garbas/vim-snipmate.git
+git clone https://github.com/honza/vim-snippets.git

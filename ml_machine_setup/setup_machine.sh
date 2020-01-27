cbd42b711e04680bc157b90b1f0ed3bc22d4c221
David Lindner <dev@davidlindner.me>
2020-01-27 15:33:05 -0800

    direnv download

--- ml_machine_setup/setup_machine.sh
+++ ml_machine_setup/setup_machine.sh
@@ -5,7 +5,9 @@ mkdir ~/bin
 cp ml-tmux.sh ~/bin/ml-tmux
 chmod +x ~/bin/ml-tmux
 
-curl https://github.com/direnv/direnv/releases/download/v2.20.0/direnv.linux-amd64 > ~/bin/direnv
+cd ~/bin/
+wget https://github.com/direnv/direnv/releases/download/v2.20.0/direnv.linux-amd64
+mv direnv.linux-amd64 direnv
 chmod +x ~/bin/direnv
 
 git clone https://github.com/david-lindner/nvidia-htop ~/nvidia-htop

93b2ef66e6ed855e1b7df39e0f41bbec28bb7af3
David Lindner <dev@davidlindner.me>
2021-04-30 20:14:10 +0200

    update ml-tmux

--- ml_machine_setup/ml-tmux.sh
+++ ml_machine_setup/ml-tmux.sh
@@ -5,4 +5,48 @@ tmux new-session \; \
   split-window -v \; \
   send-keys "nvidia-htop" C-m \; \
   select-pane -t 0 \; \
+  new-window -a \; \
+  split-window -h \; \
+  split-window -v \; \
+  split-window -v \; \
+  select-pane -U \; \
+  select-pane -U \; \
+  split-window -v \; \
+  select-pane -L \; \
+  split-window -v \; \
+  split-window -v \; \
+  select-pane -U \; \
+  select-pane -U \; \
+  split-window -v \; \
+  select-pane -U \; \
+  new-window -a \; \
+  split-window -h \; \
+  split-window -v \; \
+  split-window -v \; \
+  select-pane -U \; \
+  select-pane -U \; \
+  split-window -v \; \
+  select-pane -L \; \
+  split-window -v \; \
+  split-window -v \; \
+  select-pane -U \; \
+  select-pane -U \; \
+  split-window -v \; \
+  select-pane -U \; \
+  new-window -a \; \
+  split-window -h \; \
+  split-window -v \; \
+  split-window -v \; \
+  select-pane -U \; \
+  select-pane -U \; \
+  split-window -v \; \
+  select-pane -L \; \
+  split-window -v \; \
+  split-window -v \; \
+  select-pane -U \; \
+  select-pane -U \; \
+  split-window -v \; \
+  select-pane -U \; \
+  select-window -t 0
+
 
